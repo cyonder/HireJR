031eee34b06435934433ab74d54f2adf850af784
Cagdas Yonder <cagdasyonder@gmail.com>
2018-05-05 00:49:51 -0400

    Deploy Heroku

--- client/src/constants/systemTypes.js
+++ client/src/constants/systemTypes.js
@@ -1,4 +1,4 @@
 // SYSTEM
-export const ROOT_API_URL = 'http://localhost:5000/api';
-// export const ROOT_API_URL = 'http://hirejr.com/api';
+// export const ROOT_API_URL = 'http://localhost:5000/api';
+export const ROOT_API_URL = 'http://hirejr.com/api';
 export const AUTHENTICATION_TOKEN = 'authentication_token'
